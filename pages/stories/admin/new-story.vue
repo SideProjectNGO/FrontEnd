4304684f8fb5ba067bb6c51f2817e49b804d36b1
adnanmadi417 <adnanmadi417@gmail.com>
2025-01-28 01:36:05 +0800

    created new story form but did not finalize it yet

---
+++ pages/stories/admin/new-story.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "new-story"
+})
+</script>
+
+<template>
+  <AdminNavBar/>
+  <NewStoryFrom/>
+  <AdminFooter/>
+</template>
+
+<style scoped>
+
+</style>
