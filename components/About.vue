b42311a1e877a998482d5179ec7865d2f56d7dda
adnanmadi417 <adnanmadi417@gmail.com>
2025-01-28 09:49:54 +0800

    write clean code

--- components/About.vue
+++ components/About.vue
@@ -1,36 +1,50 @@
 <script setup lang="ts">
 
+const aboutItem = [
+  {
+    id: 1,
+    title: "About Us",
+    content: "At MyChild, we are a dedicated team of local and international individuals passionate about nurturing and empowering children. Our diverse team includes core members and educators who share a unified goal: ensuring that every child has the opportunity to thrive.",
+    image: "/images/about-us.webp",
+  },
+  {
+    id: 2,
+    title: "Our Vision",
+    content: "A world where every child, regardless of their background, enjoys equal access to opportunities for growth, education, and happiness. We envision a future where no child is left behind, where they are protected from harm, empowered to dream, and nurtured to become compassionate, confident leaders of tomorrow.",
+    image: "/images/vision.jpeg",
+  },
+  {
+    id: 3,
+    title: "Our Mission",
+    content: "Our mission is to provide holistic care, education, and support to children in need. Through community-driven programs, advocacy, and sustainable solutions, we aim to ensure every child has access to a safe, nurturing environment and the resources they need to unlock their full potential.",
+    image: "/images/mission.webp",
+  }
+];
+
 </script>
 
 <template>
   <div class="about">
-    <h1 class="title">More about Mychild</h1>
-    <div class="container">
-      <div class="box">
-        <h2 class="sub-title">Vision</h2>
-        <p class="description">
-          A world where every child, regardless of their background, enjoys equal access to
-          opportunities for growth, education, and happiness. We envision a future where no child is left behind, where
-          they are protected from harm, empowered to dream, and nurtured to become compassionate, confident leaders of
-          tomorrow.
-        </p>
-      </div>
-      <div class="box">
-        <img src="../public/images/vision.jpeg" alt="vision">
-      </div>
-    </div>
-    <div class="container">
-      <div class="box">
-        <img src="../public/images/vision.jpeg" alt="vision">
-      </div>
-      <div class="box">
-        <h2 class="sub-title">Mission</h2>
-        <p class="description">
-          Our mission is to provide holistic care, education, and support to children in need.
-          Through community-driven programs, advocacy, and sustainable solutions, we aim to ensure every child has
-          access to a safe, nurturing environment and the resources they need to unlock their full potential.
-        </p>
-      </div>
+    <h1 class="title">More about MyChild</h1>
+    <div class="container" v-for="item in aboutItem" :key="item.id">
+      <template v-if="item.id % 2 !== 0">
+        <div class="item-box">
+          <h2 class="sub-title">{{ item.title }}</h2>
+          <p class="description">{{ item.content }}</p>
+        </div>
+        <div class="image-box">
+          <img :src="item.image" :alt="item.title" />
+        </div>
+      </template>
+      <template v-else>
+        <div class="image-box">
+          <img :src="item.image" :alt="item.title" />
+        </div>
+        <div class="item-box">
+          <h2 class="sub-title">{{ item.title }}</h2>
+          <p class="description">{{ item.content }}</p>
+        </div>
+      </template>
     </div>
   </div>
 </template>
@@ -71,3 +85,4 @@
 }
 
 </style>
+
