09a1edd364c4c9ba20c037ca48109ee659204e2c
adnanmadi417 <adnanmadi417@gmail.com>
2025-02-13 12:15:44 +0800

    Ready for deployment

--- components/Test.vue
+++ components/Test.vue
@@ -1,21 +1,9 @@
-<script setup>
-import { loadStripe } from "@stripe/stripe-js";
-
-const stripePromise = loadStripe("your-public-key-here");
-
-const donate = async () => {
-  const stripe = await stripePromise;
-  const { error } = await stripe.redirectToCheckout({
-    lineItems: [{ price: "price_id_here", quantity: 1 }],
-    mode: "payment",
-    successUrl: "https://yourwebsite.com/success",
-    cancelUrl: "https://yourwebsite.com/cancel",
-  });
-
-  if (error) console.error(error);
-};
-</script>
-
 <template>
-  <button @click="donate">Donate Now</button>
+  <div>
+    <DonateButton />
+  </div>
 </template>
+
+<script setup>
+import DonateButton from '@/components/DonateButton.vue';
+</script>
