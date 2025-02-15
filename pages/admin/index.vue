eafc8ba16334ab26ef8f9bd0a8268855f14479ea
adnanmadi417 <adnanmadi417@gmail.com>
2025-02-15 14:49:40 +0800

    created admin folder

---
+++ pages/admin/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+import AdminOverview from "~/components/AdminOverview.vue";
+
+definePageMeta({
+  middleware: 'auth',
+});
+
+</script>
+
+<template>
+  <AdminNavBar/>
+  <AdminOverview/>
+  <AdminFooter/>
+</template>
+
+<style scoped>
+
+</style>
